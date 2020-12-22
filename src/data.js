a9bf4e7bf6dd64c3e98b5f9b27318b821cfec3ed
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-12-21 19:30:55 -0700

    UI Design complete

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Renee Vincelli-Evans",
+    age: 30,
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/122458182_10157392099511283_5102256130180240907_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=5GfYiR2jM_UAX8Idm5P&_nc_ht=scontent.fapa1-1.fna&oh=f225bdb240fe6a34c9657af3ef0273b7&oe=6006E609",
+  },
+  {
+    id: 2,
+    name: "Luna Rae Vincelli-Evans",
+    age: 1,
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/131307917_2101884839945919_7694038603738872618_o.jpg?_nc_cat=106&ccb=2&_nc_sid=730e14&_nc_ohc=iIwLhKziN8oAX-H5MHD&_nc_ht=scontent.fapa1-1.fna&oh=adc9dfa426aadcf18b3552476c1cdee9&oe=6005C223",
+  },
+  {
+    id: 3,
+    name: "Austin Vincelli-Evans",
+    age: 29,
+    image:
+      "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/121619176_2041913392609731_5869671214843247918_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=jF4eoZxYneMAX_37vPF&_nc_ht=scontent.fapa1-2.fna&oh=de5f72edf089645b2792321671a56f5d&oe=60074263",
+  },
+  {
+    id: 4,
+    name: "Linda Schley",
+    age: 34,
+    image:
+      "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/296437_10150407002809257_2103963637_n.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=v6cbYEbWOLUAX8Cv5e8&_nc_ht=scontent.fapa1-1.fna&oh=b2ea83c9e1576e590040b06f5236d06a&oe=60050DDC",
+  },
+  {
+    id: 5,
+    name: "Dominique McCulley",
+    age: 33,
+    image:
+      "https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/95660306_10163405054715304_522552951324016640_n.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=IkbfzQUUn8sAX-9Tzmh&_nc_ht=scontent.fapa1-2.fna&oh=8300c4650483cd08276ffddf3701d876&oe=6007751A",
+  },
+];
